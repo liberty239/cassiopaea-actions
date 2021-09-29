35de461b1d18c4d9aad9e88b74950ba9feb3d81b
liberty239 <91454880+liberty239@users.noreply.github.com>
2021-09-29 12:00:04 +0200

    Add google-site-verification meta tag to the merged HTML file.

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 #!/bin/bash
 go get -u github.com/liberty239/cassiopaea-tools/./...
 cass-src fetch-all .build/html
-cass-gen epub .build/html .build/sessions.epub
-cass-gen html .build/html .build/sessions.html
+cass-gen epub .build/html .build/sessions.epub 
+cass-gen html .build/html .build/sessions.html --meta-tags google-site-verification:$GOOGLE_SITE_VERIFICATION
 cp .build/sessions.html index.html
