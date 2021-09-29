1cb3abdd3d3faef09015935631f1a45d90d13cd5
liberty239 <91454880+liberty239@users.noreply.github.com>
2021-09-29 12:30:35 +0200

    Add Yandex verification meta tags to the merged HTML file.

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 go get -u github.com/liberty239/cassiopaea-tools/./...
 cass-src fetch-all .build/html
 cass-gen epub .build/html .build/sessions.epub 
-cass-gen html .build/html .build/sessions.html --meta-tags google-site-verification:$GOOGLE_SITE_VERIFICATION
+cass-gen html .build/html .build/sessions.html --meta-tags google-site-verification:$GOOGLE_SITE_VERIFICATION,yandex-verification:$YANDEX_VERIFICATION
 cp .build/sessions.html index.html
