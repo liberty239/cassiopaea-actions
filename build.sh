1e76213d63fa70308a0850d8eb461287d314185d
liberty239 <91454880+liberty239@users.noreply.github.com>
2021-09-29 13:15:18 +0200

    Add sitemap.xml

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ go get -u github.com/liberty239/cassiopaea-tools/./...@master
 cass-src fetch-all .build/html
 cass-gen epub .build/html .build/sessions.epub 
 cass-gen html .build/html .build/sessions.html --meta-tags google-site-verification:$GOOGLE_SITE_VERIFICATION,yandex-verification:$YANDEX_VERIFICATION
-cp .build/sessions.html index.html
+cp .build/sessions.html .site/index.html
