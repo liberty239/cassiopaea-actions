f5eb1d568cd926e0a2f17cc701a90d6d8bdbe269
liberty239 <91454880+liberty239@users.noreply.github.com>
2021-09-29 12:39:34 +0200

    Instruct go get to fetch from head.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #!/bin/bash
-go get -u github.com/liberty239/cassiopaea-tools/./...
+go get -u github.com/liberty239/cassiopaea-tools/./...@master
 cass-src fetch-all .build/html
 cass-gen epub .build/html .build/sessions.epub 
 cass-gen html .build/html .build/sessions.html --meta-tags google-site-verification:$GOOGLE_SITE_VERIFICATION,yandex-verification:$YANDEX_VERIFICATION
